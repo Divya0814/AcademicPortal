fbf44fa3e4e19fff8daaa92fd9f03d43984a6738
Divya0814 <160851752+Divya0814@users.noreply.github.com>
2026-06-20 15:30:01 +0530

    Backend

--- server.js
+++ server.js
@@ -23,18 +23,15 @@ const nodemailer = require('nodemailer');//library for sending mails
 require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-});
-/*const io = new Server(server, {
   cors: { origin: "*" } 
-}); */
-//const PORT = 5000;
-const PORT = process.env.PORT || 5000;
+}); 
+const PORT = 5000;
 
 const SECRET_KEY = process.env.SECRET_KEY;
 //app.use(cors());
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -1953,7 +1950,9 @@ app.get("/api/admin/students-filter", (req, res) => {
  app.post("/api/fingerprint/scan",(req, res) => {
   const { roll_no } = req.body;
 
-  const exePath = process.env.FP_APP_PATH || "";
+    const exePath =
+    "C:\\Users\\Yaswanth\\FingerprintApp48\\bin\\Debug\\net48\\FingerprintApp48.exe";
+
 
   const process = spawn(exePath, ["enroll", roll_no]);
 
