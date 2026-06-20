c9665252c1121b914e40d1177f3e15032b912f10
Divya0814 <160851752+Divya0814@users.noreply.github.com>
2026-06-20 12:04:12 +0530

    updated server.js and fixed issues

--- server.js
+++ server.js
@@ -24,12 +24,17 @@ require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" } 
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
-const PORT = 5000;
+/*const io = new Server(server, {
+  cors: { origin: "*" } 
+}); */
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const SECRET_KEY = process.env.SECRET_KEY;
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -1948,8 +1953,7 @@ app.get("/api/admin/students-filter", (req, res) => {
  app.post("/api/fingerprint/scan",(req, res) => {
   const { roll_no } = req.body;
 
-  const exePath =
-    "C:\\Users\\Yaswanth\\FingerprintApp48\\bin\\Debug\\net48\\FingerprintApp48.exe";
+  const exePath = process.env.FP_APP_PATH || "";
 
   const process = spawn(exePath, ["enroll", roll_no]);
 
